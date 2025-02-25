636dcdb61c5bd0449cc8864a775127b8696ce001
Anand V <42205127+vanand46@users.noreply.github.com>
2025-02-25 19:36:04 +0000

    - Corrected main.tf

--- main.tf
+++ main.tf
@@ -15,18 +15,31 @@ module "ssh_keypair" {
 }
 
 module "security_group" {
-  source            = "./modules/security-group"
-  vpc_id            = module.vpc.vpc_id
+  source = "./modules/security-group"
+  vpc_id = module.vpc.vpc_id
 }
 
 module "instance" {
-  source             = "./modules/instance"
-  subnet_id          = module.vpc.subnet_id
-  security_group_id  = module.security_group.security_group_id
-  key_name           = module.ssh_keypair.key_name
-  private_key_pem    = module.ssh_keypair.private_key_pem
+  source            = "./modules/instance"
+  subnet_id         = module.vpc.subnet_id
+  security_group_id = module.security_group.security_group_id
+  key_name          = module.ssh_keypair.key_name
+  private_key_pem   = module.ssh_keypair.private_key_pem
 }
 
 output "instance_public_ip" {
   value = module.instance.public_ip
+}
+
+resource "null_resource" "ansible_provision" {
+  # Ensure this runs after instance creation
+  depends_on = [module.instance]
+
+  provisioner "local-exec" {
+    command = <<EOT
+echo "[infra_vm]" > inventory.ini
+echo "${module.instance.public_ip}" >> inventory.ini
+ansible-playbook -i inventory.ini ansible/playbook.yml
+EOT
+  }
 }
