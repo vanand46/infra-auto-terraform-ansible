56da566582b60d80c7b77c502191f41534fe6e57
Anand V <42205127+vanand46@users.noreply.github.com>
2025-02-25 14:29:24 +0000

    - Added the ssh-keypair module

---
+++ modules/ssh-keypair/main.tf
@@ -0,0 +1,17 @@
+# Generate an RSA Private Key
+resource "tls_private_key" "infra_rsa" {
+  algorithm = "RSA"
+}
+
+# Create an AWS Key Pair
+resource "aws_key_pair" "infra_key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.infra_rsa.public_key_openssh
+}
+
+# Store the Private Key Locally
+resource "local_file" "infra_private_key" {
+  content      = tls_private_key.infra_rsa.private_key_pem
+  filename     = var.key_file_name
+  file_permission = "0400"
+}
---
+++ modules/ssh-keypair/variables.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  type    = string
+  default = "infra-key"
+}
+
+variable "key_file_name" {
+  type    = string
+  default = "infra-key.pem"
+}
